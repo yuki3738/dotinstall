e147dd6b17de19722997001e86c7c6dc68829ed4
yuki3738 <y.minamiya@gmail.com>
2017-12-03 20:47:46 +0900

    JavaScriptでカウントダウンタイマーを作ろう #02 JavaScriptを書く準備をしよう

---
+++ go_dutch_js_v3/MyApp/js/main.js
@@ -0,0 +1,9 @@
+(function() {
+  'use strict';
+
+  var timer = document.getElementById('timer');
+  var min = document.getElementById('min');
+  var sec = document.getElementById('sec');
+  var reset = document.getElementById('reset');
+  var start = document.getElementById('start');
+})();
