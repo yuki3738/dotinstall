1f5cb6b28dc9a9ddb2b242d8edef40ed8751750d
yuki3738 <y.minamiya@gmail.com>
2018-06-24 23:57:58 +0900

    07 残り時間をセットしてみよう

--- countdown_js/MyTimer/js/main.js
+++ countdown_js/MyTimer/js/main.js
@@ -3,13 +3,13 @@
 
   var timer = document.getElementById('timer');
   var min = document.getElementById('min');
-  var sec= document.getElementById('sec');
+  var sec = document.getElementById('sec');
   var reset = document.getElementById('reset');
   var start = document.getElementById('start');
 
   var startTime;
   var timeLeft;
-  var timeToCountDown = 4 * 1000;
+  var timeToCountDown = 0;
   var timerId;
 
   function updateTimer(t) {
@@ -41,5 +41,20 @@
   start.addEventListener('click', function() {
     startTime = Date.now();
     countDown();
+  });
+
+  min.addEventListener('click', function() {
+    timeToCountDown += 60 * 1000;
+    updateTimer(timeToCountDown);
+  });
+
+  sec.addEventListener('click', function() {
+    timeToCountDown += 1000;
+    updateTimer(timeToCountDown);
+  });
+
+  reset.addEventListener('click', function() {
+    timeToCountDown = 0;
+    updateTimer(timeToCountDown);
   });
 })();
