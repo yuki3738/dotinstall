87a0a1d1fa647229db00c1f5ef5b2bc255aef96a
Yuki Minamiya <y.minamiya@gmail.com>
2018-09-17 19:19:33 +0900

    05 JavaScriptを書く準備をしよう

---
+++ slot_js_v4/MySlot/js/main.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+
+  var panels = document.getElementsByClassName('panel');
+  var spin = document.getElementById('spin');
+
+  var cards = [
+    'seven.png',
+    'bell.png',
+    'cberry.png',
+  ];
+
+  spin.addEventListener('click', function() {
+
+  });
+})();
