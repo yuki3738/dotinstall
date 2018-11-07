57f9d1b5912723721584a7b908e1290f880c7e15
Yuki Minamiya <y.minamiya@gmail.com>
2018-11-07 21:27:44 +0900

    Moment.js入門 #06 公式サイトを参照してみよう

--- basic_momentjs/MyMomentJS/js/main.js
+++ basic_momentjs/MyMomentJS/js/main.js
@@ -1,10 +1,7 @@
 (() => {
   'use strict';
 
-  // const d = moment();
-  // const d = moment('2018-01-10');
-  // const d = moment('01-10-2018', 'MM-DD-YYYY');
-  // const d = moment(new Date(2018, 0, 10));
-  const d = moment([2018, 0, 10]);
-  console.log(d.format('LLLL'));
+  const d = moment('2018-01-10');
+  d.locale('en');
+  console.log(d.format('MMMM Do, dddd'));
 })();
