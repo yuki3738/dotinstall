758f99d0b9435655107b75488b14a091f3b549fc
Yuki Minamiya <y.minamiya@gmail.com>
2018-11-07 21:20:16 +0900

    Moment.js入門 #04 Moment.jsで書き直してみよう

--- basic_momentjs/MyMomentJS/js/main.js
+++ basic_momentjs/MyMomentJS/js/main.js
@@ -6,7 +6,7 @@
   const p = document.querySelector('p');
 
   button.addEventListener('click', () => {
-    const d = new Date(input.value);
-    p.textContent = `${d.getFullYear()}年${d.getMonth() + 1}月${d.getDate()}日`;
+    const d = moment(input.value);
+    p.textContent = d.isValid() ? d.format('LL') : '無効な日付です';
   });
 })();
