058684666bec44ca179aeed95634d73700c69dcc
Yuki Minamiya <y.minamiya@gmail.com>
2018-11-11 18:29:51 +0900

    Moment.js入門 #09 get()で日時情報を取得しよう

--- basic_momentjs/MyMomentJS/js/main.js
+++ basic_momentjs/MyMomentJS/js/main.js
@@ -2,21 +2,24 @@
   'use strict';
 
   const d = moment('2018-01-10 15:35:24');
-  // 15:35:24
-  // console.log(d.format('HH:mm:ss'));
-  // console.log(d.format('hh:mm:ss a'));
-  // d.locale('en');
-  // console.log(d.format('l'));
-  // console.log(d.format('ll'));
-  // console.log(d.format('lll'));
-  // console.log(d.format('llll'));
-  // console.log(d.format('L'));
-  // console.log(d.format('LL'));
-  // console.log(d.format('LLL'));
-  // console.log(d.format('LLLL'));
-  // console.log(moment('2018-01').daysInMonth());
-  // console.log(moment('2018-02').daysInMonth());
-  console.log(d.toArray());
-  console.log(d.toObject());
-  console.log(d.format('[Today]: L'));
+  // console.log(d.get('year'));
+  // console.log(d.get('month')); // 0-11
+  // console.log(d.get('date'));
+  // console.log(d.get('day')); // 0-6
+  // console.log(d.get('hour'));
+  // console.log(d.get('minute'));
+  // console.log(d.get('second'));
+  // console.log(d.get('millisecond'));
+  // console.log(d.get('y'));
+  // console.log(d.get('M')); // 0-11
+  // console.log(d.get('D'));
+  // console.log(d.get('d')); // 0-6
+  // console.log(d.get('H'));
+  // console.log(d.get('h'));
+  // console.log(d.get('m'));
+  // console.log(d.get('s'));
+  // console.log(d.get('ms'));
+  console.log(d.year());
+  console.log(d.month());
+  console.log(d.date());
 })();
