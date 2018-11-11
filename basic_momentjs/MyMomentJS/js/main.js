9a9b2fdee832c9add1d1f358a3e743f68e5ba5cc
Yuki Minamiya <y.minamiya@gmail.com>
2018-11-11 18:23:04 +0900

    Moment.js入門 #07 format()を使って表記を指定しよう

--- basic_momentjs/MyMomentJS/js/main.js
+++ basic_momentjs/MyMomentJS/js/main.js
@@ -1,7 +1,17 @@
 (() => {
   'use strict';
 
-  const d = moment('2018-01-10');
+  const d = moment('2018-01-10 15:35:24');
+  // 15:35:24
+  // console.log(d.format('HH:mm:ss'));
+  // console.log(d.format('hh:mm:ss a'));
   d.locale('en');
-  console.log(d.format('MMMM Do, dddd'));
+  console.log(d.format('l'));
+  console.log(d.format('ll'));
+  console.log(d.format('lll'));
+  console.log(d.format('llll'));
+  console.log(d.format('L'));
+  console.log(d.format('LL'));
+  console.log(d.format('LLL'));
+  console.log(d.format('LLLL'));
 })();
