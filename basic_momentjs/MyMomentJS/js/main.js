c871b9587a08a39dc511367c75c1bc980c021f98
Yuki Minamiya <y.minamiya@gmail.com>
2018-11-11 18:27:14 +0900

    Moment.js入門 #08 日時に関する情報を取得しよう

--- basic_momentjs/MyMomentJS/js/main.js
+++ basic_momentjs/MyMomentJS/js/main.js
@@ -5,13 +5,18 @@
   // 15:35:24
   // console.log(d.format('HH:mm:ss'));
   // console.log(d.format('hh:mm:ss a'));
-  d.locale('en');
-  console.log(d.format('l'));
-  console.log(d.format('ll'));
-  console.log(d.format('lll'));
-  console.log(d.format('llll'));
-  console.log(d.format('L'));
-  console.log(d.format('LL'));
-  console.log(d.format('LLL'));
-  console.log(d.format('LLLL'));
+  // d.locale('en');
+  // console.log(d.format('l'));
+  // console.log(d.format('ll'));
+  // console.log(d.format('lll'));
+  // console.log(d.format('llll'));
+  // console.log(d.format('L'));
+  // console.log(d.format('LL'));
+  // console.log(d.format('LLL'));
+  // console.log(d.format('LLLL'));
+  // console.log(moment('2018-01').daysInMonth());
+  // console.log(moment('2018-02').daysInMonth());
+  console.log(d.toArray());
+  console.log(d.toObject());
+  console.log(d.format('[Today]: L'));
 })();
