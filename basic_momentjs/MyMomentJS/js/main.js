67fb621584e25b994f793ba6ece3d6f8998ecc5d
Yuki Minamiya <y.minamiya@gmail.com>
2018-11-12 20:21:54 +0900

    Moment.js入門 #13 日時の差を計算してみよう

--- basic_momentjs/MyMomentJS/js/main.js
+++ basic_momentjs/MyMomentJS/js/main.js
@@ -1,10 +1,12 @@
 (() => {
   'use strict';
 
-  const d = moment('2018-01-10 15:35:24');
-  // console.log(d.clone().add(3, 'day').format('LLLL'));
-  // console.log(d.clone().subtract(3, 'year').format('LLLL'));
-  console.log(d.clone().add(1, 'y').add(2, 'M').format('LLLL'));
-  console.log(d.clone().add({year: 1, month: 2}).format('LLLL'));
-  console.log(d.clone().add({y: 1, M: 2}).format('LLLL'));
+  // const start = moment('2018-01-10 08:00:00');
+  // const end = moment('2018-01-10 10:30:00');
+  // // console.log(end.diff(start));
+  // // console.log(end.diff(start, 'hour'));
+  // console.log(end.diff(start, 'hour', true));
+  // const posted = moment('2018-01-10 08:00:00');
+  const posted = moment('2018-07-11 06:50:00');
+  console.log(posted.fromNow());
 })();
