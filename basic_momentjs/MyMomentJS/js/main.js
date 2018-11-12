bcc12dd68519f9546a353869c0b61975ab35e3cc
Yuki Minamiya <y.minamiya@gmail.com>
2018-11-12 20:13:11 +0900

    Moment.js入門 #11 clone()で値をコピーしてみよう

--- basic_momentjs/MyMomentJS/js/main.js
+++ basic_momentjs/MyMomentJS/js/main.js
@@ -2,24 +2,8 @@
   'use strict';
 
   const d = moment('2018-01-10 15:35:24');
+  // console.log(d.set('year', 2020).format('LLLL'));
   // console.log(d.get('year'));
-  // console.log(d.get('month')); // 0-11
-  // console.log(d.get('date'));
-  // console.log(d.get('day')); // 0-6
-  // console.log(d.get('hour'));
-  // console.log(d.get('minute'));
-  // console.log(d.get('second'));
-  // console.log(d.get('millisecond'));
-  // console.log(d.get('y'));
-  // console.log(d.get('M')); // 0-11
-  // console.log(d.get('D'));
-  // console.log(d.get('d')); // 0-6
-  // console.log(d.get('H'));
-  // console.log(d.get('h'));
-  // console.log(d.get('m'));
-  // console.log(d.get('s'));
-  // console.log(d.get('ms'));
-  console.log(d.year());
-  console.log(d.month());
-  console.log(d.date());
+  console.log(d.clone().set('year', 2020).format('LLLL'));
+  console.log(d.get('year'));
 })();
