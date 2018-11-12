5905f2716debd8d79fa7924ccb0cf79892345a7c
Yuki Minamiya <y.minamiya@gmail.com>
2018-11-12 20:16:35 +0900

    Moment.js入門 #12 add()､subtract()を使ってみよう

--- basic_momentjs/MyMomentJS/js/main.js
+++ basic_momentjs/MyMomentJS/js/main.js
@@ -2,8 +2,9 @@
   'use strict';
 
   const d = moment('2018-01-10 15:35:24');
-  // console.log(d.set('year', 2020).format('LLLL'));
-  // console.log(d.get('year'));
-  console.log(d.clone().set('year', 2020).format('LLLL'));
-  console.log(d.get('year'));
+  // console.log(d.clone().add(3, 'day').format('LLLL'));
+  // console.log(d.clone().subtract(3, 'year').format('LLLL'));
+  console.log(d.clone().add(1, 'y').add(2, 'M').format('LLLL'));
+  console.log(d.clone().add({year: 1, month: 2}).format('LLLL'));
+  console.log(d.clone().add({y: 1, M: 2}).format('LLLL'));
 })();
