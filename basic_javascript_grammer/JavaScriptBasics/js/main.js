b5d65e0124d884819bbb2ee9a7c6aae1ab1cd0de
yuki3738 <y.minamiya@gmail.com>
2019-02-04 23:53:00 +0900

    06 数値の計算をしてみよう

--- basic_javascript_grammer/JavaScriptBasics/js/main.js
+++ basic_javascript_grammer/JavaScriptBasics/js/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('hello' + 'world');
+// console.log(10 + 3); // 13
+// console.log(10 - 3); // 7
+// console.log(10 * 3); // 30
+// console.log(10 / 3); // 3.333....
+// console.log(10 % 3); // 1
+// console.log(10 ** 3); // 1000
+
+console.log(2 + 10 * 3); // 32
+console.log((2 + 10) * 3); // 36
