fd602ddb14cecd9d858670353d5e3a0c225677b8
yuki3738 <y.minamiya@gmail.com>
2019-03-18 23:59:51 +0900

    詳解JavaScript 基礎文法編 #13 条件演算子を使ってみよう

--- basic_javascript_grammer/JavaScriptBasics/js/main.js
+++ basic_javascript_grammer/JavaScriptBasics/js/main.js
@@ -1,13 +1,11 @@
 'use strict';
 
-// if
+const score = 85;
 
-const score = 40;
+// if (score >= 80) {
+//   console.log('Great!');
+// } else {
+//   console.log('OK...!');
+// }
 
-if (score >= 80) {
-  console.log('Great!');
-} else if (score >= 60) {
-  console.log('Good');
-} else {
-  console.log('OK');
-}
+score >= 80 ? console.log('Great!') : console.log('OK...!');
