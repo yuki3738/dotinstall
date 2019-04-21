abef37ba817239f6d204374ead74df19c8b200aa
yuki3738 <y.minamiya@gmail.com>
2019-04-21 23:00:40 +0900

    詳解JavaScript 基礎文法編 #16 forを使ってループ処理をしよう

--- basic_javascript_grammer/JavaScriptBasics/js/main.js
+++ basic_javascript_grammer/JavaScriptBasics/js/main.js
@@ -1,27 +1,5 @@
 'use strict';
 
-const signal = 'pink';
-
-// if (signal === 'red') {
-//   console.log('stop!');
-// } else if (signal === 'yellow') {
-//   console.log('caution!');
-// } else if (signal === 'blue'){
-//   console.log('go!');
-// }
-
-switch (signal) {
-  case 'red':
-    console.log('stop!');
-    break;
-  case 'yellow':
-    console.log('caution!');
-    break;
-  case 'blue':
-  case 'green':
-    console.log('go!');
-    break;
-  default:
-    console.log('wrong signal!');
-    break;
+for (let i = 1; i <= 10; i++) {
+  console.log(`${i} hello`);
 }
