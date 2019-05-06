04870877faf37388478e05dfb067485aef5e6f88
yuki3738 <y.minamiya@gmail.com>
2019-05-06 22:24:45 +0900

    JavaScriptでおみくじを作ろう #07 配列を使ってみよう

--- omikuji_js_v4/MyOmikuji/js/main.js
+++ omikuji_js_v4/MyOmikuji/js/main.js
@@ -4,18 +4,9 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
-    const n = Math.floor(Math.random() * 3);
-    switch (n) {
-      case 0:
-        btn.textContent = '大吉';
-        break;
-      case 1:
-        btn.textContent = '中吉';
-        break;
-      case 2:
-        btn.textContent = '凶';
-        break;
-    }
+    const results = ['大吉', '中吉', '凶', '末吉'];
+    const n = Math.floor(Math.random() * results.length);
+    btn.textContent = results[n]
   });
   btn.addEventListener('mousedown', () => {
     btn.classList.add('pressed');
