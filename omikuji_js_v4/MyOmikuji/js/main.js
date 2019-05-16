c663b39124062fd9f7cb14f9fce2c63e06088ce9
yuki3738 <y.minamiya@gmail.com>
2019-05-16 22:58:01 +0900

    JavaScriptでおみくじを作ろう #08 確率を操作してみよう

--- omikuji_js_v4/MyOmikuji/js/main.js
+++ omikuji_js_v4/MyOmikuji/js/main.js
@@ -4,9 +4,14 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
-    const results = ['大吉', '中吉', '凶', '末吉'];
-    const n = Math.floor(Math.random() * results.length);
-    btn.textContent = results[n]
+    const n = Math.random();
+    if (n < 0.034) {
+      btn.textContent = '大吉';
+    } else if (n < 0.66) {
+      btn.textContent = '中吉';
+    } else {
+      btn.textContent = '凶';
+    }
   });
   btn.addEventListener('mousedown', () => {
     btn.classList.add('pressed');
