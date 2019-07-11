144577aede752a823cab974201a21066d587a8c9
yuki3738 <y.minamiya@gmail.com>
2019-07-12 00:22:26 +0900

    JavaScriptでストップウォッチを作ろう #02 画面のマークアップをしよう

---
+++ stop_watch_js_v5/MyStopwatch/js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+{
+  const timer = document.getElementById('timer');
+  const start = document.getElementById('start');
+  const stop = document.getElementById('timer');
+  const reset = document.getElementById('reset');
+}
