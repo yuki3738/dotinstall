c76fd87351ac23b471421dde91a90cdac132da48
yuki3738 <y.minamiya@gmail.com>
2019-10-20 21:39:39 +0900

    はじめてのPython » #08 ループ処理を使ってみよう

--- basic_python_v4/main.py
+++ basic_python_v4/main.py
@@ -2,14 +2,14 @@ import random
 
 answer = random.randint(1, 10)
 
-print("Your guess? ", end="")
-guess = int(input())
+while True:
+    print("Your guess? ", end="")
+    guess = int(input())
 
-if answer == guess:
-    print("Bingo!")
-elif answer > guess:
-    print("Bigger!")
-else:
-    print("Smaller!")
-
-print("[Answer: %i]" %answer)
+    if answer == guess:
+        print("Bingo!")
+        break
+    elif answer > guess:
+        print("Bigger!")
+    else:
+        print("Smaller!")
