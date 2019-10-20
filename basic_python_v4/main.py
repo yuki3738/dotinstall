baedeccccfe0020a75d096420b386ce959e95231
yuki3738 <y.minamiya@gmail.com>
2019-10-20 21:35:32 +0900

    はじめてのPython » #07 条件を追加してみよう

--- basic_python_v4/main.py
+++ basic_python_v4/main.py
@@ -1,9 +1,15 @@
-answer = 6
+import random
+
+answer = random.randint(1, 10)
 
 print("Your guess? ", end="")
 guess = int(input())
 
 if answer == guess:
     print("Bingo!")
+elif answer > guess:
+    print("Bigger!")
 else:
-    print("Boo...")
+    print("Smaller!")
+
+print("[Answer: %i]" %answer)
