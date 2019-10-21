d855a6460a3b1b2f76e23130715fe0aa08382efc
yuki3738 <y.minamiya@gmail.com>
2019-10-21 11:20:38 +0900

    はじめてのPython » #09 正解までの回数を表示しよう

--- basic_python_v4/main.py
+++ basic_python_v4/main.py
@@ -1,13 +1,15 @@
 import random
 
 answer = random.randint(1, 10)
+count = 0
 
 while True:
     print("Your guess? ", end="")
     guess = int(input())
+    count += 1
 
     if answer == guess:
-        print("Bingo!")
+        print("Bingo! It took %i guesses!" %count)
         break
     elif answer > guess:
         print("Bigger!")
