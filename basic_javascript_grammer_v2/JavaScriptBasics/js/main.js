5bd61c0870b013027dae6376a866b711498b4ff5
yuki3738 <y.minamiya@gmail.com>
2019-12-07 23:30:18 +0900

    詳解JavaScript 基礎文法編 » #05 文字列を扱ってみよう

--- basic_javascript_grammer_v2/JavaScriptBasics/js/main.js
+++ basic_javascript_grammer_v2/JavaScriptBasics/js/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log('Hello world from main.js!');
+// console.log('hello');
+// console.log("hello");
+
+// console.log('it's me!');
+// console.log("it's me!");
+// console.log('it\'s me!');
+
+// console.log('hel\nlo wor\tld');
+
+console.log('hello' + 'world');
