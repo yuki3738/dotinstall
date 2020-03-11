519469756fb3b33c7969c2f718fcd76dd3606f04
yuki3738 <y.minamiya@gmail.com>
2020-03-11 21:32:11 +0900

    詳解JavaScript 基礎文法編 » #08 変数を使ってみよう

--- basic_javascript_grammer_v2/JavaScriptBasics/js/main.js
+++ basic_javascript_grammer_v2/JavaScriptBasics/js/main.js
@@ -1,6 +1,12 @@
 'use strict';
 
-const price = 150;
+// 定数 const
+// 変数 let var
 
+let price = 150;
+console.log(price * 140);
+console.log(price * 160);
+
+price = 170;
 console.log(price * 140);
 console.log(price * 160);
