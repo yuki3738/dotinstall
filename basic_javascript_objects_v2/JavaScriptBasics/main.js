39698094724f2b1cba619d727bec509205f71af0
yuki3738 <y.minamiya@gmail.com>
2020-03-16 23:34:41 +0900

    詳解JavaScript オブジェクト編 » #07 分割代入を使ってみよう

--- basic_javascript_objects_v2/JavaScriptBasics/main.js
+++ basic_javascript_objects_v2/JavaScriptBasics/main.js
@@ -1,14 +1,22 @@
 'use strict';
 
 {
-  const otherScores = [10, 20];
-  const scores = [80, 90, 40, 70, ...otherScores];
-  // console.log(scores);
+  const scores = [80, 90, 40, 70];
 
-  function sum(a, b) {
-    console.log(a + b);
-  }
+  // const [a, b, c, d] = scores;
+  // console.log(a);
+  // console.log(b);
+  // console.log(c);
+  // console.log(d);
 
-  sum(...otherScores);
-  // sum(10, 20);
+  // const [a, b, ...others] = scores;
+  // console.log(a);
+  // console.log(b);
+  // console.log(others);
+
+  let x = 30;
+  let y = 70;
+  [x, y] = [y, x];
+  console.log(x);
+  console.log(y);
 }
