2f2fd019e93133c52627ddf77178674433c3d762
yuki3738 <y.minamiya@gmail.com>
2020-03-17 23:12:57 +0900

    詳解JavaScript オブジェクト編 » #12 プロパティを操作してみよう

--- basic_javascript_objects_v2/JavaScriptBasics/main.js
+++ basic_javascript_objects_v2/JavaScriptBasics/main.js
@@ -1,12 +1,18 @@
 'use strict';
 
 {
-  // const point = [100, 180];
-
-  // const point = {x: 100, y: 180};
   const point = {
     x: 100,
     y: 180,
   };
+
+  point.x = 120;
+  // point['x'] = 120;
+
+  // console.log(point.x);
+  // console.log(point['y']);
+
+  point.z = 90;
+  delete point.y;
   console.log(point);
 }
