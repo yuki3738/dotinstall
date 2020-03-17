4e3f9ba9a53e538e3e5159ba5196e8068fd8508d
yuki3738 <y.minamiya@gmail.com>
2020-03-17 23:15:09 +0900

    詳解JavaScript オブジェクト編 » #13 オブジェクトを操作してみよう

--- basic_javascript_objects_v2/JavaScriptBasics/main.js
+++ basic_javascript_objects_v2/JavaScriptBasics/main.js
@@ -1,18 +1,20 @@
 'use strict';
 
 {
+  const otherProps = {
+    r: 4,
+    color: 'red',
+  };
+
   const point = {
     x: 100,
     y: 180,
+    ...otherProps,
   };
+  // console.log(point);
 
-  point.x = 120;
-  // point['x'] = 120;
-
-  // console.log(point.x);
-  // console.log(point['y']);
-
-  point.z = 90;
-  delete point.y;
-  console.log(point);
+  const {x, r, ...others} = point;
+  console.log(x);
+  console.log(r);
+  console.log(others);
 }
