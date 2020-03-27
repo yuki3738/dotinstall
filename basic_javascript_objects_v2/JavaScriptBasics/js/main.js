4dfa1c3a4067b26690c185930f8695dc65a64133
yuki3738 <y.minamiya@gmail.com>
2020-03-27 20:46:17 +0900

    詳解JavaScript オブジェクト編 » #18 join()､split()を使ってみよう

--- basic_javascript_objects_v2/JavaScriptBasics/js/main.js
+++ basic_javascript_objects_v2/JavaScriptBasics/js/main.js
@@ -1,13 +1,15 @@
 'use strict';
 
 {
-  const str = 'hello';
+  const d = [2019, 11, 14];
 
-  // console.log(str.length);
+  // console.log(d.join('/'));
+  // console.log(d.join(''));
 
-  // str.substring(開始位置, 終了位置);
-  // console.log(str.substring(2, 4));
-
-  console.log(str[1]);
-  // str[1] = 'a';
+  const t = '17:08:24';
+  // console.log(t.split(':'));
+  const [hour, minute, second] = t.split(':');
+  console.log(hour);
+  console.log(minute);
+  console.log(second);
 }
