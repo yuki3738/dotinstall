4630d015cd40d8def16b7bdfb8e831924e065759
yuki3738 <y.minamiya@gmail.com>
2020-03-28 13:30:54 +0900

    詳解JavaScript オブジェクト編 » #21 現在日時を扱ってみよう

--- basic_javascript_objects_v2/JavaScriptBasics/js/main.js
+++ basic_javascript_objects_v2/JavaScriptBasics/js/main.js
@@ -1,14 +1,7 @@
 'use strict';
 
 {
-  // console.log(Math.random());
-
-  // 0, 1, 2
-  // Math.floor(Math.random() * 3)
-  // 0, ..., n
-  // Math.floor(Math.random() * (n + 1))
-  // min, ..., max
-  // Math.floor(Math.random() * (max + 1 - min)) + min
-
-  console.log(Math.floor(Math.random() * 6) + 1);
+  const d = new Date();
+  console.log(d);
+  console.log(`${d.getMonth() + 1}月 ${d.getDate()}日`);
 }
