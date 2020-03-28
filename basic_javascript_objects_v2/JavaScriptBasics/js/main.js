c6aba9b36045ec7a54012d35c768ca1c302a17e4
yuki3738 <y.minamiya@gmail.com>
2020-03-28 16:17:31 +0900

    詳解JavaScript オブジェクト編 » #34 クラスを拡張したい場合を考えよう

--- basic_javascript_objects_v2/JavaScriptBasics/js/main.js
+++ basic_javascript_objects_v2/JavaScriptBasics/js/main.js
@@ -15,17 +15,31 @@
       this.likeCount++;
       this.show();
     }
+  }
+  class SponsoredPost {
+    constructor(text, sponsor) {
+      this.text = text;
+      this.likeCount = 0;
+      this.sponsor = sponsor;
+    }
 
-    static showInfo() {
-      console.log('Post class version 1.0');
+    show() {
+      console.log(`${this.text} - ${this.likeCount} likes`);
+      console.log(`... sponsored by ${this.sponsor}`);
+    }
+
+    like() {
+      this.likeCount++;
+      this.show();
     }
   }
 
   const posts = [
     new Post('javascriptの勉強中…'),
     new Post('プログラミング楽しい！'),
+    new SponsoredPost('3分動画でマスターしよう', 'dotinstall'),
   ];
 
-  posts[0].like();
-  Post.showInfo();
+  posts[2].show();
+  posts[2].like();
 }
