dd777e379a012f44cfafc748b34ea8806edae363
yuki3738 <y.minamiya@gmail.com>
2020-03-28 13:34:09 +0900

    詳解JavaScript オブジェクト編 » #22 特定の日時を扱ってみよう

--- basic_javascript_objects_v2/JavaScriptBasics/js/main.js
+++ basic_javascript_objects_v2/JavaScriptBasics/js/main.js
@@ -1,7 +1,9 @@
 'use strict';
 
 {
-  const d = new Date();
+  const d = new Date(2020, 3);
+  d.setHours(10, 20, 30);
+  d.setDate(31);
+  d.setDate(d.getDate() + 3);
   console.log(d);
-  console.log(`${d.getMonth() + 1}月 ${d.getDate()}日`);
 }
