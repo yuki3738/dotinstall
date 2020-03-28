35a47d34ecb2a3bffb9dc891239b939951fcf699
yuki3738 <y.minamiya@gmail.com>
2020-03-28 14:35:59 +0900

    詳解JavaScript オブジェクト編 » #25 setTimeout()を使ってみよう

--- basic_javascript_objects_v2/JavaScriptBasics/js/main.js
+++ basic_javascript_objects_v2/JavaScriptBasics/js/main.js
@@ -1,14 +1,16 @@
 'use strict';
 
 {
-  let i = 0;
+  let i = 0
+
   function showTime() {
     console.log(new Date());
+    const timeoutId = setTimeout(showTime, 1000);
     i++;
     if (i > 2) {
-      clearInterval(intervalId);
+      clearTimeout(timeoutId);
     }
   }
 
-  const intervalId = setInterval(showTime, 1000);
+  showTime();
 }
