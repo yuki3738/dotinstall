5f7352d6b2526ebd84a6aacad28114b249a4f115
yuki3738 <y.minamiya@gmail.com>
2020-03-28 19:10:13 +0900

    詳解JavaScript DOM編 » #03 複数の要素を取得してみよう

--- basic_javascript_dom_v2/JavaScriptBasics/js/main.js
+++ basic_javascript_dom_v2/JavaScriptBasics/js/main.js
@@ -2,9 +2,9 @@
 
 {
   function update() {
-    // document.querySelector('h1').textContent = 'Changed!';
-    // document.querySelector('#target').textContent = 'Changed!';
-    document.getElementById('target').textContent = 'Changed!';
+    document.querySelectorAll('p').forEach((p, index) => {
+      p.textContent = `${index}番目のpです！`;
+    });
   }
 
   setTimeout(update, 1000);
