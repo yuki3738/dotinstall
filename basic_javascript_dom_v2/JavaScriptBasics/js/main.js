5f1fd4da1eeb694ad97ccf741f470f0c5e371130
yuki3738 <y.minamiya@gmail.com>
2020-03-29 15:46:00 +0900

    詳解JavaScript DOM編 » #09 カスタムデータ属性を扱ってみよう

--- basic_javascript_dom_v2/JavaScriptBasics/js/main.js
+++ basic_javascript_dom_v2/JavaScriptBasics/js/main.js
@@ -4,12 +4,6 @@
   document.querySelector('button').addEventListener('click', () => {
     const targetNode = document.getElementById('target');
 
-    // targetNode.classList.add('my-color');
-    // if (targetNode.classList.contains('my-color') === true) {
-    //   targetNode.classList.remove('my-color');
-    // } else {
-    //   targetNode.classList.add('my-color');
-    // }
-    targetNode.classList.toggle('my-color');
+    targetNode.textContent = targetNode.dataset.translation;
   });
 }
