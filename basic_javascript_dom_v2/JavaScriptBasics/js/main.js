cee3d25fa3f0d870560968f3ac62b6eac34e6054
yuki3738 <y.minamiya@gmail.com>
2020-03-29 15:42:29 +0900

    詳解JavaScript DOM編 » #08 classListを使ってみよう

--- basic_javascript_dom_v2/JavaScriptBasics/js/main.js
+++ basic_javascript_dom_v2/JavaScriptBasics/js/main.js
@@ -4,6 +4,12 @@
   document.querySelector('button').addEventListener('click', () => {
     const targetNode = document.getElementById('target');
 
-    targetNode.className = 'my-color my-border';
+    // targetNode.classList.add('my-color');
+    // if (targetNode.classList.contains('my-color') === true) {
+    //   targetNode.classList.remove('my-color');
+    // } else {
+    //   targetNode.classList.add('my-color');
+    // }
+    targetNode.classList.toggle('my-color');
   });
 }
