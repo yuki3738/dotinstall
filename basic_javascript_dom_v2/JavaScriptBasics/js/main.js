9656d2b6d4f14f87523cc0e591dea33b2b571fdb
yuki3738 <y.minamiya@gmail.com>
2020-03-29 16:36:12 +0900

    詳解JavaScript DOM編 » #11 要素の複製､ 挿入をしてみよう

--- basic_javascript_dom_v2/JavaScriptBasics/js/main.js
+++ basic_javascript_dom_v2/JavaScriptBasics/js/main.js
@@ -2,10 +2,11 @@
 
 {
   document.querySelector('button').addEventListener('click', () => {
-    const item2 = document.createElement('li');
-    item2.textContent = 'item 2';
+    const item0 = document.querySelectorAll('li')[0];
+    const copy = item0.cloneNode(true);
 
     const ul = document.querySelector('ul');
-    ul.appendChild(item2);
+    const item2 = document.querySelectorAll('li')[2];
+    ul.insertBefore(copy, item2);
   });
 }
