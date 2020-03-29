ed27f229b4567ec7792819643dbbed43a08af250
yuki3738 <y.minamiya@gmail.com>
2020-03-29 19:29:35 +0900

    詳解JavaScript DOM編 » #20 フォームを送信してみよう

--- basic_javascript_dom_v2/JavaScriptBasics/js/main.js
+++ basic_javascript_dom_v2/JavaScriptBasics/js/main.js
@@ -1,22 +1,8 @@
 'use strict';
 
 {
-  const text = document.querySelector('textarea');
-
-  // text.addEventListener('focus', () => {
-  //   console.log('focus');
-  // });
-
-  // text.addEventListener('blur', () => {
-  //   console.log('blur');
-  // });
-
-  text.addEventListener('input', () => {
-    // console.log('input');
-    console.log(text.value.length);
-  });
-
-  text.addEventListener('change', () => {
-    console.log('change');
+  document.querySelector('form').addEventListener('submit', e => {
+    e.preventDefault();
+    console.log('submit');
   });
 }
