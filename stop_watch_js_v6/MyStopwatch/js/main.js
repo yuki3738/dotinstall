167067c18dc24ab50646ad10ae8fa76bec682904
yuki3738 <y.minamiya@gmail.com>
2020-04-04 02:51:51 +0900

    JavaScriptでストップウォッチを作ろう » #04 タイマーを表示させよう

--- stop_watch_js_v6/MyStopwatch/js/main.js
+++ stop_watch_js_v6/MyStopwatch/js/main.js
@@ -10,6 +10,12 @@
 
   function countUp() {
     console.log(Date.now() - startTime);
+    const d = new Date(Date.now() - startTime);
+    const m = String(d.getMinutes()).padStart(2, '0');
+    const s = String(d.getSeconds()).padStart(2, '0');
+    const ms = String(d.getMilliseconds()).padStart(3, '0');
+    timer.textContent = `${m}:${s}:${ms}`;
+
 
     setTimeout(() => {
       countUp();
