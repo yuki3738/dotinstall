91dc87d4703957c649febf343d22f5fc8c8525dd
yuki3738 <y.minamiya@gmail.com>
2020-04-10 01:36:35 +0900

    JavaScriptでタイピングゲームを作ろう » #06 正解とミスの数をカウントしよう

--- typing_js_v3/MyTypingGame/js/main.js
+++ typing_js_v3/MyTypingGame/js/main.js
@@ -3,8 +3,13 @@
 {
   const word = 'apple';
   let loc = 0;
+  let score = 0;
+  let miss = 0;
 
   const target = document.getElementById('target');
+  const scoreLabel = document.getElementById('score');
+  const missLabel = document.getElementById('miss');
+
   target.textContent = word;
 
   window.addEventListener('keydown', e => {
@@ -12,8 +17,12 @@
     if (e.key === word[loc]) {
       console.log('score');
       loc++;
+      score++;
+      scoreLabel.textContent = score;
     } else {
       console.log('miss');
+      miss++;
+      missLabel.textContent = miss;
     }
   });
 }
