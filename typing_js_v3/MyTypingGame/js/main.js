a13777f90c9a14ddec40be6f61944db5fa546199
yuki3738 <y.minamiya@gmail.com>
2020-04-11 18:13:45 +0900

    JavaScriptでタイピングゲームを作ろう » #08 複数の単語に対応させよう

--- typing_js_v3/MyTypingGame/js/main.js
+++ typing_js_v3/MyTypingGame/js/main.js
@@ -1,11 +1,18 @@
 'use strict';
 
 {
-  const word = 'apple';
   let loc = 0;
   let score = 0;
   let miss = 0;
 
+  const words = [
+    'apple',
+    'sky',
+    'blue',
+    'middle',
+    'set',
+  ];
+  let word = words[Math.floor(Math.random() * words.length)];
   const target = document.getElementById('target');
   const scoreLabel = document.getElementById('score');
   const missLabel = document.getElementById('miss');
@@ -24,6 +31,10 @@
     console.log(e.key);
     if (e.key === word[loc]) {
       loc++;
+      if (loc === word.length) {
+        word = words[Math.floor(Math.random() * words.length)];
+        loc = 0;
+      }
       updateTarget();
       score++;
       scoreLabel.textContent = score;
