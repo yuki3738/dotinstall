dfa339729cb1c4cb3dc180ca16cf3bc0a830bb75
yuki3738 <y.minamiya@gmail.com>
2020-04-11 18:27:27 +0900

    JavaScriptでタイピングゲームを作ろう » #09 ゲームをスタートさせよう

--- typing_js_v3/MyTypingGame/js/main.js
+++ typing_js_v3/MyTypingGame/js/main.js
@@ -17,8 +17,6 @@
   const scoreLabel = document.getElementById('score');
   const missLabel = document.getElementById('miss');
 
-  target.textContent = word;
-
   function updateTarget() {
     let placeholder = '';
     for (let i = 0; i < loc; i++) {
@@ -26,6 +24,10 @@
     }
     target.textContent = placeholder + word.substring(loc);
   }
+
+  window.addEventListener('click', () => {
+    target.textContent = word;
+  });
 
   window.addEventListener('keydown', e => {
     console.log(e.key);
