84179c626de01a97e2fac07c5568a6ccc60b8bb4
yuki3738 <y.minamiya@gmail.com>
2020-10-20 23:59:58 +0900

    JavaScriptでビンゴシートを作ろう » #07 すべての列の配列を完成させよう

--- bingo_js/MyBingo/js/main.js
+++ bingo_js/MyBingo/js/main.js
@@ -1,15 +1,27 @@
 'use strict';
 
 {
-  const source = [];
-  for (let i = 0; i < 15; i++) {
-    source[i] = i + 1;
+  function createColumn(col) {
+    const source = [];
+    for (let i = 0; i < 15; i++) {
+      source[i] = i + 1 + 15 * col;
+    }
+
+    const column = [];
+    for (let i = 0; i < 5; i++) {
+      column[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+    }
+
+    return column;
   }
 
-  const b = [];
-  for (let i = 0; i < 5; i++) {
-    b[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  }
+  const columns = [];
+  columns[0] = createColumn(0);
+  columns[1] = createColumn(1);
+  columns[2] = createColumn(2);
+  columns[3] = createColumn(3);
+  columns[4] = createColumn(4);
+  columns[2][2] = 'FREE';
 
-  console.log(b);
+  console.table(columns);
 }
