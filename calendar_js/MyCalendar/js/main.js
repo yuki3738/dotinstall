d9d0d87e7feea5b871c92bbef8cc6f4dda3fd8a1
yuki3738 <y.minamiya@gmail.com>
2020-11-02 00:42:47 +0900

    JavaScriptでカレンダーを作ろう » #06 今月分の日付を取得しよう

--- calendar_js/MyCalendar/js/main.js
+++ calendar_js/MyCalendar/js/main.js
@@ -3,4 +3,17 @@
 {
   const year = 2020;
   const month = 4; // 5月
+
+  function getCalendarBody() {
+    const dates = [];
+    const lastDate = new Date(year, month + 1, 0).getDate();
+
+    for (let i = 1; i <= lastDate; i++) {
+      dates.push(i);
+    }
+
+    console.log(dates);
+  }
+
+  getCalendarBody();
 }
