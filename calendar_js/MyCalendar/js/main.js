d203ae60d41af2765fb11833dcdd7288ab15b762
yuki3738 <y.minamiya@gmail.com>
2020-11-29 15:58:02 +0900

    JavaScriptでカレンダーを作ろう » #09 前月分の日付を取得しよう

--- calendar_js/MyCalendar/js/main.js
+++ calendar_js/MyCalendar/js/main.js
@@ -6,6 +6,22 @@ console.clear();
   const year = 2020;
   const month = 4; // 5月
 
+  function getCalendarHead() {
+    const dates = [];
+    const d = new Date(year, month, 0).getDate();
+    const n = new Date(year, month, 1).getDay();
+
+    for (let i = 0; i < n; i++) {
+      dates.unshift({
+        date: d - i,
+        isToday: false,
+        isDisabled: true,
+      });
+    }
+
+    console.log(dates);
+  }
+
   function getCalendarBody() {
     const dates = [];
     const lastDate = new Date(year, month + 1, 0).getDate();
@@ -22,4 +38,5 @@ console.clear();
   }
 
   getCalendarBody();
+  getCalendarHead();
 }
