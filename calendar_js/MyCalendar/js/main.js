714695dcd4d27e150884b3b5408df5d36372fd94
yuki3738 <y.minamiya@gmail.com>
2020-11-29 18:59:36 +0900

    JavaScriptでカレンダーを作ろう » #15 年月も表示してみよう

--- calendar_js/MyCalendar/js/main.js
+++ calendar_js/MyCalendar/js/main.js
@@ -53,6 +53,15 @@ console.clear();
   }
 
   function createCalendar() {
+    const tbody = document.querySelector('tbody');
+
+    while (tbody.firstChild) {
+      tbody.removeChild(tbody.firstChild);
+    }
+
+    const title = `${year}/${String(month + 1).padStart(2, '0')}`;
+    document.getElementById('title').textContent = title;
+
     const dates = [
       ...getCalendarHead(),
       ...getCalendarBody(),
